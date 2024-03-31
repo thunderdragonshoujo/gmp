d3da9831c7b138a5042581e9710c9ae78f0fa352
barlennan2bb <barlennan2bb@gmail.com>
2024-03-31 14:21:19 -0500

    updated images for the carousel

--- src/app/ShadCaro.tsx
+++ src/app/ShadCaro.tsx
@@ -13,9 +13,12 @@ import {
     return(
   <Carousel className='container' opts={{loop:true}} plugins={[Autoplay()]}>
   <CarouselContent className="items-center w-full">
-    <CarouselItem><Image src='/2M229H.JPG' height='600' width='600' alt="img" /></CarouselItem>
-    <CarouselItem><Image src='/0_90MF.JPG' height='600' width='600' alt="img"/></CarouselItem>
-    <CarouselItem><Image src='/GLC_14-104_1_4.JPG' height='600' width='600' alt="img"/></CarouselItem>
+    <CarouselItem><Image src='/Globe-med.png' height='400' width='400' alt="Globe"/></CarouselItem>
+    <CarouselItem><Image src='/2M229H.JPG' height='600' width='600' alt="Magnetron" /></CarouselItem>
+    <CarouselItem><Image src='/0_90MF.JPG' height='400' width='400' alt="Capacitor"/></CarouselItem>
+    <CarouselItem><Image src='/GLC_0526.JPG' height='400' width='400' alt="Diode"/></CarouselItem>
+    <CarouselItem><Image src='/tile-05.jpg' height='500' width='500' alt="Fuses"/></CarouselItem>
+    <CarouselItem><Image src='/tile-04.jpg' height='500' width='500' alt="Switches"/></CarouselItem>
   </CarouselContent>
   <CarouselPrevious />
   <CarouselNext />
