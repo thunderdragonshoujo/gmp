c40070a476a39f30ed061eb86ca9fec0ff9a89f7
clint <thunderdragonshoujo@gmail.com>
2024-04-07 05:31:26 -0500

    fix embla import and removed export outputs

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -5,6 +5,7 @@ import useEmblaCarousel, {
   type UseEmblaCarouselType,
 } from "embla-carousel-react"
 import { ArrowLeft, ArrowRight } from "lucide-react"
+
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 
