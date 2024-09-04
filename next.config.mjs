9beb0964ef3a0956d3a58e5219019b0612b4532b
thunderdragonshoujo <32719118+thunderdragonshoujo@users.noreply.github.com>
2024-09-03 22:53:17 -0500

    toggle eslinting next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ images: {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: false,
+    ignoreDuringBuilds: true,
   },
 };
 
